14c127d40b5bfd18cfff1f3f2e7ec9008277ec3e
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2024-12-23 17:53:25 +0400

    Added classes Reviewer and Lecturer

--- Homework student_and_mentor.py
+++ Homework student_and_mentor.py
@@ -6,12 +6,25 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress \
+        and course in lecturer.courses_attached:
+            if course in lecturer.lector_grades:
+                lecturer.lector_grades[course] += [grade]
+            else:
+                lecturer.lector_grades[course] = [grade]
+        else:
+            return "Ошибка"
+
+
 class Mentor:
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
         self.courses_attached = []
 
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached \
         and course in student.courses_in_progress:
@@ -22,18 +35,24 @@ class Mentor:
         else:
             return "Ошибка"
         
+class Lecturer(Mentor):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.lector_grades = {}
+        
 
 some_student = Student("Ruoy", "Eman")
 some_student.courses_in_progress += ["Python"]
  
-cool_mentor = Mentor("Some", "Buddy")
-cool_mentor.courses_attached += ["Python"]
+some_lecturer = Lecturer("Some", "Buddy")
+some_lecturer.courses_attached += ["Python"]
  
-cool_mentor.rate_hw(some_student, "Python", 10)
-cool_mentor.rate_hw(some_student, "Python", 10)
-cool_mentor.rate_hw(some_student, "Python", 10)
- 
-print(some_student.grades)
+some_student.rate_lecturer(some_lecturer, "Python", 10)
+some_student.rate_lecturer(some_lecturer, "Python", 8)
+some_student.rate_lecturer(some_lecturer, "Python", 5)
+
+
+print(some_lecturer.lector_grades)
     
 
 
