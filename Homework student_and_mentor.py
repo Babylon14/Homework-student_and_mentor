019197d24b3c6314157a296b89302ede2339eca2
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2024-12-23 18:09:56 +0400

    Added magic method __str__ for classes

--- Homework student_and_mentor.py
+++ Homework student_and_mentor.py
@@ -16,6 +16,14 @@ class Student:
         else:
             return "Ошибка"
 
+    def __str__(self):
+        return (f"Имя: {self.first_name}\n"
+                f"Фамилия: {self.last_name}\n"
+                f"Средняя оценка за домашние задания: {9.9}\n"
+                f"Курсы в процессе изучения: {', '.join(self.courses_in_progress)}\n"
+                f"Завершенные курсы: {", ".join(self.finished_courses)}")
+
+
 
 class Mentor:
     def __init__(self, first_name, last_name):
@@ -35,24 +43,37 @@ class Reviewer(Mentor):
         else:
             return "Ошибка"
         
+    def __str__(self):
+        return (f"Имя: {self.first_name}\n"
+                f"Фамилия: {self.last_name}")
+        
 class Lecturer(Mentor):
     def __init__(self, first_name, last_name):
         super().__init__(first_name, last_name)
         self.lector_grades = {}
-        
 
+    def __str__(self):
+        return (f"Имя: {self.first_name}\n"
+                f"Фамилия: {self.last_name}\n"
+                f"Средняя оценка за лекции: {9.9}")
+
+    
 some_student = Student("Ruoy", "Eman")
-some_student.courses_in_progress += ["Python"]
- 
+some_student.courses_in_progress += ["Python", "Git"]
+some_student.finished_courses += ["Введение в программирование"]
+
 some_lecturer = Lecturer("Some", "Buddy")
 some_lecturer.courses_attached += ["Python"]
+
+some_reviewer = Reviewer("Some", "Budy")
+some_reviewer.courses_attached += ["Python"]
  
-some_student.rate_lecturer(some_lecturer, "Python", 10)
-some_student.rate_lecturer(some_lecturer, "Python", 8)
-some_student.rate_lecturer(some_lecturer, "Python", 5)
 
-
-print(some_lecturer.lector_grades)
+print(some_student)
+print()
+print(some_lecturer)
+print()
+print(some_reviewer)
     
 
 
